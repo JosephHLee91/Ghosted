f5bec39a7211c357c7b4fa59470681b48dd603fa
Amir <your.gwandu151@gmail.com>
2023-06-01 14:34:05 -0400

    added changes to jwtconverter

--- server/ghosted/src/main/java/learn/ghosted/security/JwtConverter.java
+++ server/ghosted/src/main/java/learn/ghosted/security/JwtConverter.java
@@ -3,6 +3,7 @@ package learn.ghosted.security;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import learn.ghosted.models.AppUser;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
