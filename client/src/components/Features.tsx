611d4dd03fc50c4120556d8a9aa1d8bea8a952aa
Andrew Robles <AndrewJordanRobles@gmail.com>
2023-06-05 16:18:39 -0400

    Finished homepage with exception of videos

--- client/src/components/Features.tsx
+++ client/src/components/Features.tsx
@@ -6,7 +6,46 @@ const Features = () => {
           Features
         </h2>
 
-        <div className='mt-6 grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8'></div>
+        <div className='mx-auto max-w-screen-xl px-4 py-16 flex flex-row'>
+          <div className='w-3/6'>
+            <p className='text-left text-gray-700 text-2xl font-bold tracking-tight'>
+              Data driven
+            </p>
+            <p className='text-left text-gray-500 mt-4'>
+              Track more than <span className='italic'>just</span> quantity of
+              applications.{' '}
+              <span className='text-indigo-500 font-bold'>Ghosted</span> serves
+              job application data in easy to understand, powerful data charts
+              to put you in control of your job search.
+            </p>
+          </div>
+          <div className='w-3/6 flex'>
+            <div className='w-1/3 flex items-center shrink-0 grow justify-center lg:grow-0'>
+              VIDEO PLACEHOLDER
+            </div>
+          </div>
+        </div>
+
+        <div className='mx-auto max-w-screen-xl px-4 py-16 flex flex-row'>
+          <div className='w-3/6 flex'>
+            <div className='w-1/3 flex items-center shrink-0 grow justify-center lg:grow-0'>
+              VIDEO PLACEHOLDER
+            </div>
+          </div>
+          <div className='w-3/6'>
+            <p className='text-left text-gray-700 text-2xl font-bold tracking-tight'>
+              Convenient organization
+            </p>
+            <p className='text-left text-gray-500 mt-4'>
+              Juggling hundreds of emails? Getting swamped by spreadsheet
+              information? Get rid of all the unecessary steps and track the
+              necessary information you need with our comprehensive user
+              dashboard. Ensure no application goes unnoticed or forgotten,
+              empowering you to stay organized and informed throughout your
+              search.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
